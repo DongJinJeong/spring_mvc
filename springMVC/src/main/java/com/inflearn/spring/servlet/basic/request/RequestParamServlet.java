9ea3ae0a7dd381ec6801a0f7f6ad8fe332283e5b
DongJinJeong <ehd5719@gmail.com>
2021-03-21 17:22:51 +0900

    RequestHeaderServlet

---
+++ springMVC/src/main/java/com/inflearn/spring/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,20 @@
+package com.inflearn.spring.servlet.basic.request;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+}
