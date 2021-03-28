06f12dd97199222e739e314833521d4860295743
DongJinJeong <ehd5719@gmail.com>
2021-03-28 21:35:39 +0900

    adapter

---
+++ demo/src/main/java/com/example/demo/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -0,0 +1,17 @@
+package com.example.demo.servlet.web.frontcontroller.v5;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.example.demo.servlet.web.frontcontroller.ModelView;
+
+public interface MyHandlerAdapter {
+	
+	boolean supports(Object handler);
+
+	ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException;
+	
+}
